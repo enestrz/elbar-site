6e22312ac044095889373523cdd7347c9d0e91ef
Enes Terzi <enestrz15@gmail.com>
2024-03-26 21:35:36 +0300

    Update dependencies and improve rendering in Model component

--- app/urunlerimiz/interaktif/Model.tsx
+++ app/urunlerimiz/interaktif/Model.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default function Model() {
@@ -13,22 +13,40 @@ export default function Model() {
 
         // Scene
         const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff);
 
         // Camera
         const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-        camera.position.z = 5;
+        camera.position.z = 3;
+
+        const light = new THREE.AmbientLight(0xffffff, 3); // soft white light
+        light.castShadow = true;
+        scene.add(light);
 
         // Renderer
-        const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
-        renderer.setClearColor(0x000000);
+        const renderer = new THREE.WebGLRenderer({
+            canvas: canvasRef.current,
+            antialias: true,
+            alpha: true,
+            powerPreference: "high-performance",
+            stencil: true,
+            depth: false,
+        });
+        renderer.outputColorSpace = THREE.SRGBColorSpace;
+        renderer.setClearColor(0x000000, 0);
+        renderer.toneMapping = THREE.ReinhardToneMapping;
+        renderer.setPixelRatio(window.devicePixelRatio);
         resizeRendererToDisplaySize(renderer);
         containerRef.current.appendChild(renderer.domElement);
 
-        const box = new THREE.BoxGeometry(1, 3, 0.05);
-        const material = new THREE.MeshBasicMaterial({
+        const box = new THREE.BoxGeometry(1.5, 4, 0.05);
+
+        const material = new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load(
-                "/kapilar/primerli/cift-renk/LMD 206-206-206 İTALYAN CEVİZ.png"
+                "/kapilar/primerli/cift-renk/LMD 206-206-206 İTALYAN CEVİZ.png",
+                (t) => {
+                    t.colorSpace = THREE.SRGBColorSpace;
+                    t.wrapS = t.wrapT = THREE.RepeatWrapping;
+                }
             ),
         });
 
